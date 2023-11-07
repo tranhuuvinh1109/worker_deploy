130f5a9d616c37d240b9d3f287523f7ac9a84c2c
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-11-07 15:49:09 +0700

    disable CORS

--- worker_deploy/settings.py
+++ worker_deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-25o!%wqd&!ow56@m49ty=ptew(s7w&hv=!&z1m16o@^&*o(e9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'train'
 ]
-
+CORS_ORIGIN_ALLOW_ALL=True
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
